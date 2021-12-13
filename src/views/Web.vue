11e73e08660399399a910b37f51a19374391dbc9
samzhangjy <samzhang951@outlook.com>
2021-12-13 20:49:01 +0800

    :sparkles: a few fixes to the Web component

--- src/views/Web.vue
+++ src/views/Web.vue
@@ -11,11 +11,7 @@
         subtitle="Loading..."
       />
     </div>
-    <bd-container
-      class="text-center"
-      style="margin-top: 100px"
-      v-if="!loading && error"
-    >
+    <bd-container class="text-center" style="margin-top: 100px" v-if="!loading && error">
       <h1>出错了</h1>
       <hr />
       <br />
@@ -50,14 +46,10 @@
               <h4>
                 <a :href="result.url" target="_blank">{{ result.title }}</a>
                 <span v-if="result.author">
-                  <small class="text-secondary"
-                    >&nbsp;&nbsp;{{ result.author }}</small
-                  >
+                  <small class="text-secondary">&nbsp;&nbsp;{{ result.author }}</small>
                 </span>
                 <span v-if="result.time">
-                  <small class="text-secondary"
-                    >&nbsp;&nbsp;{{ result.time }}</small
-                  >
+                  <small class="text-secondary">&nbsp;&nbsp;{{ result.time }}</small>
                 </span>
               </h4>
               <p v-if="result.des">{{ result.des }}</p>
@@ -92,11 +84,7 @@
               </a>
             </h3>
             <p class="des">
-              <span
-                v-if="result.time"
-                class="text-secondary"
-                style="font-size: 100%"
-              >
+              <span v-if="result.time" class="text-secondary" style="font-size: 100%">
                 {{ result.time }}
               </span>
               {{ result.des }}
@@ -112,9 +100,7 @@
                 :key="index"
                 style="margin-right: 15px; line-height: 30px"
               >
-                <router-link
-                  :to="`/search/web?q=${encodeURIComponent(result)}&pn=1`"
-                >
+                <router-link :to="`/search/web?q=${encodeURIComponent(result)}&pn=1`">
                   {{ result }}
                 </router-link>
               </span>
@@ -142,9 +128,7 @@
           :url="resultsTieba.url"
           coverType="image"
         >
-          <span class="text-secondary"
-            >关注人数：{{ resultsTieba.followers }}</span
-          >
+          <span class="text-secondary">关注人数：{{ resultsTieba.followers }}</span>
           <span class="text-secondary"
             >&nbsp;&nbsp;累计发帖：{{ resultsTieba.total }}</span
           >
@@ -167,9 +151,7 @@
           coverType="none"
         >
           <div v-for="(result, index) in resultsBlog.blogs" :key="index">
-            <a :href="result.url" target="_blank">
-              {{ result.title }}&nbsp;&nbsp;
-            </a>
+            <a :href="result.url" target="_blank"> {{ result.title }}&nbsp;&nbsp; </a>
             <span class="text-secondary" v-if="result.des">
               <br />{{ result.des }}&nbsp;&nbsp;
             </span>
@@ -189,9 +171,7 @@
         >
           <div>
             <h4>简介</h4>
-            <p v-if="resultsGitee.license">
-              开源协议：{{ resultsGitee.license }}
-            </p>
+            <p v-if="resultsGitee.license">开源协议：{{ resultsGitee.license }}</p>
             <p v-if="resultsGitee.lang">开发语言：{{ resultsGitee.lang }}</p>
             <span> Star：{{ resultsGitee.star }}&nbsp;&nbsp; </span>
             <span> Fork：{{ resultsGitee.fork }}&nbsp;&nbsp; </span>
@@ -278,92 +258,89 @@ export default {
   methods: {
     getResults: async function () {
       this.loading = true;
-      await SearchService.searchWeb(
-        this.query,
-        this.curPage,
-        window.location.href
-      ).then((data) => {
-        if (data == "Error") {
-          this.error = true;
-          this.loading = false;
-          return;
-        }
-        this.pages = [];
-        this.error = false;
-        this.resultsNormal = [];
-        this.resultsCalc = {};
-        this.resultsNews = null;
-        this.resultsVideo = null;
-        this.resultsBaike = null;
-        this.resultsTieba = null;
-        this.resultsExtra = false;
-        this.resultsBlog = null;
-        this.resultsGitee = null;
-        this.results = data.data.results;
-        this.totalPages = parseInt(data.data.pages);
-        for (var i = 0; i < this.results.length; i++) {
-          if (this.results[i].type === "result") {
-            this.resultsNormal.push(this.results[i]);
-          } else if (this.results[i].type === "calc") {
-            this.resultsCalc = this.results[i];
-          } else if (this.results[i].type === "news") {
-            this.resultsNews = this.results[i].results;
-          } else if (this.results[i].type === "video") {
-            this.resultsVideo = this.results[i].results;
-          } else if (this.results[i].type === "baike") {
-            this.resultsBaike = this.results[i].result;
-            this.resultsExtra = true;
-          } else if (this.results[i].type === "total") {
-            this.total = this.results[i].result;
-          } else if (this.results[i].type === "tieba") {
-            this.resultsTieba = this.results[i].result;
-            this.resultsExtra = true;
-          } else if (this.results[i].type === "blog") {
-            this.resultsBlog = this.results[i].result;
-            this.resultsExtra = true;
-          } else if (this.results[i].type === "gitee") {
-            this.resultsGitee = this.results[i].result;
-            this.resultsExtra = true;
-          } else if (this.results[i].type === "related") {
-            this.resultsRelated = this.results[i].results;
+      await SearchService.searchWeb(this.query, this.curPage, window.location.href).then(
+        (data) => {
+          if (data == "Error") {
+            this.error = true;
+            this.loading = false;
+            return;
           }
+          this.pages = [];
+          this.error = false;
+          this.resultsNormal = [];
+          this.resultsCalc = {};
+          this.resultsNews = null;
+          this.resultsVideo = null;
+          this.resultsBaike = null;
+          this.resultsTieba = null;
+          this.resultsExtra = false;
+          this.resultsBlog = null;
+          this.resultsGitee = null;
+          this.results = data.data.results;
+          this.totalPages = parseInt(data.data.pages);
+          this.total = data.data.total;
+          for (var i = 0; i < this.results.length; i++) {
+            if (this.results[i].type === "result") {
+              this.resultsNormal.push(this.results[i]);
+            } else if (this.results[i].type === "calc") {
+              this.resultsCalc = this.results[i];
+            } else if (this.results[i].type === "news") {
+              this.resultsNews = this.results[i].results;
+            } else if (this.results[i].type === "video") {
+              this.resultsVideo = this.results[i].results;
+            } else if (this.results[i].type === "baike") {
+              this.resultsBaike = this.results[i].result;
+              this.resultsExtra = true;
+            } else if (this.results[i].type === "tieba") {
+              this.resultsTieba = this.results[i].result;
+              this.resultsExtra = true;
+            } else if (this.results[i].type === "blog") {
+              this.resultsBlog = this.results[i].result;
+              this.resultsExtra = true;
+            } else if (this.results[i].type === "gitee") {
+              this.resultsGitee = this.results[i].result;
+              this.resultsExtra = true;
+            } else if (this.results[i].type === "related") {
+              this.resultsRelated = this.results[i].results;
+            }
+          }
+          this.resultId += 1;
+          this.loading = false;
+          this.getPages();
+          this.links = [
+            {
+              to: `/search/web?q=${this.query}&pn=1`,
+              text: "网页",
+              icon: "search",
+            },
+            {
+              to: `/search/news?q=${this.query}&pn=1`,
+              text: "资讯",
+              icon: "news",
+            },
+            {
+              to: `/search/video?q=${this.query}&pn=1`,
+              text: "视频",
+              icon: "video",
+            },
+            {
+              to: `/search/pic?q=${this.query}&pn=1`,
+              text: "图片",
+              icon: "image-alt",
+            },
+            {
+              to: `/search/zhidao?q=${this.query}&pn=1`,
+              text: "知道",
+              icon: "help-circle",
+            },
+            {
+              to: `/search/wenku?q=${this.query}&pn=1`,
+              text: "文库",
+              icon: "book",
+            },
+          ];
         }
-        this.resultId += 1;
-        this.loading = false;
-        this.getPages();
-        this.links = [
-          {
-            to: `/search/web?q=${this.query}&pn=1`,
-            text: "网页",
-            icon: "search",
-          },
-          {
-            to: `/search/news?q=${this.query}&pn=1`,
-            text: "资讯",
-            icon: "news",
-          },
-          {
-            to: `/search/video?q=${this.query}&pn=1`,
-            text: "视频",
-            icon: "video",
-          },
-          {
-            to: `/search/pic?q=${this.query}&pn=1`,
-            text: "图片",
-            icon: "image-alt",
-          },
-          {
-            to: `/search/zhidao?q=${this.query}&pn=1`,
-            text: "知道",
-            icon: "help-circle",
-          },
-          {
-            to: `/search/wenku?q=${this.query}&pn=1`,
-            text: "文库",
-            icon: "book",
-          },
-        ];
-      });
+      );
     },
     getPages: function () {
       const cur = parseInt(this.$route.query.pn);
@@ -387,11 +364,14 @@ export default {
   },
   watch: {
     "$route.query.q": function () {
+      if (this.$route.query.q === undefined) return;
       this.query = this.$route.query.q;
       this.curPage = Number(this.$route.query.pn);
       this.getResults();
     },
     "$route.query.pn": function () {
+      if (this.$route.query.q === undefined || isNaN(parseInt(this.$route.query.pn)))
+        return;
       this.query = this.$route.query.q;
       this.curPage = Number(this.$route.query.pn);
       this.getResults();
